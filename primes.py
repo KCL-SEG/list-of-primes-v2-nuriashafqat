f7871ec00f3886293ddc971bd9241b98d7049798
nuriashafqat <146842113+nuriashafqat@users.noreply.github.com>
2023-10-19 11:03:51 +0100

    Raise ValueError if input is less than or equal to 0

--- primes.py
+++ primes.py
@@ -1,6 +1,27 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def is_prime(number):
+    if number < 2:
+        return False
+    if number == 2:
+        return True
+    if number % 2 == 0:
+        return False
+    for i in range(3, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
 def primes(number_of_primes):
     list = []
-    return list
+    number = 2
+
+    if(number_of_primes <= 0):
+        raise ValueError()
+    else:
+        while len(list) < number_of_primes:
+            if is_prime(number):
+                list.append(number)
+            number += 1
+        return list
